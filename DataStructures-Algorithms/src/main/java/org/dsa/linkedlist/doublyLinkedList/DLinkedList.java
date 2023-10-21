235de11613d019dfac623849c6b3144ec535720d
ShivamWadkar <shivamvwadkar@gmail.com>
2023-10-21 17:25:11 +0530

    Added manipulation code for doubly linked list

--- DataStructures-Algorithms/src/main/java/org/dsa/linkedlist/doublyLinkedList/DLinkedList.java
+++ DataStructures-Algorithms/src/main/java/org/dsa/linkedlist/doublyLinkedList/DLinkedList.java
@@ -6,7 +6,7 @@ public class DLinkedList {
 
     public void insertFirst(int data) {
         DNode newNode = new DNode(data);
-        if(head == null) {
+        if (head == null) {
             head = newNode;
             tail = newNode;
         } else {
@@ -16,11 +16,127 @@ public class DLinkedList {
         }
     }
 
+    public void insertLast(int data) {
+        if (head == null) {
+            insertFirst(data);
+        } else {
+            DNode newNode = new DNode(data);
+            newNode.setPrev(tail);
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+    }
+
+    public void insertByPosition(int data, int pos) {
+        if (pos == 1) {
+            insertFirst(data);
+        } else if (pos == noOfNodes() + 1) {
+            insertLast(data);
+        } else if (pos > 1 && pos < noOfNodes() + 1) {
+            DNode newNode = new DNode(data);
+            DNode trav = head;
+            int count = 1;
+            while (count < pos - 1) {
+                trav = trav.getNext();
+                count++;
+            }
+            newNode.setPrev(trav);
+            newNode.setNext(trav.getNext());
+            trav.getNext().setPrev(newNode);
+            trav.setNext(newNode);
+        } else {
+            System.out.println("Invalid Position");
+        }
+    }
+
+    public void deleteFirst() {
+        if (head == tail) {
+            System.out.println("DLL is empty");
+        } else if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            DNode temp = head;
+            head = head.getNext();
+            head.setPrev(null);
+            temp.setNext(null);
+        }
+    }
+
+    public void deleteLast() {
+        if (head == tail) {
+            System.out.println("DLL is empty");
+        } else if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            DNode temp = tail;
+            tail = tail.getPrev();
+            tail.setNext(null);
+            temp.setPrev(null);
+        }
+    }
+
+    public void deleteByPosition(int pos) {
+        if (pos == 1) {
+            deleteFirst();
+        } else if (pos == noOfNodes()) {
+            deleteLast();
+        } else if (pos > 1 && pos < noOfNodes()) {
+            DNode trav = head;
+            int count = 1;
+            while (count < pos - 1) {
+                trav = trav.getNext();
+                count++;
+            }
+            DNode temp = trav.getNext();
+            trav.setNext(temp.getNext());
+            temp.getNext().setPrev(trav);
+            temp.setNext(null);
+            temp.setPrev(null);
+        }
+    }
+
+    public void reverse() {
+        DNode trav = head;
+        while (trav != null) {
+            DNode temp = trav.getNext();
+            trav.setNext(trav.getPrev());
+            trav.setPrev(temp);
+            trav = temp;
+        }
+        DNode temp = head;
+        head = tail;
+        tail = temp;
+    }
+
+    public void empty() {
+        DNode trav = head;
+        DNode travn = head.getNext();
+        while (travn != null) {
+            trav.setNext(null);
+            trav.setPrev(null);
+            trav = travn;
+            travn = travn.getNext();
+        }
+        head = tail = null;
+    }
+
+    private int noOfNodes() {
+        DNode trav = head;
+        int count = 0;
+        while (trav != null) {
+            trav = trav.getNext();
+            count++;
+        }
+        return count;
+    }
+
     @Override
     public String toString() {
         String str = "";
         DNode trav = head;
-        while(trav != null) {
+        while (trav != null) {
             str += trav;
             trav = trav.getNext();
         }
--- DataStructures-Algorithms/src/main/java/org/dsa/linkedlist/doublyLinkedList/UserOfDLinkedList.java
+++ DataStructures-Algorithms/src/main/java/org/dsa/linkedlist/doublyLinkedList/UserOfDLinkedList.java
@@ -7,6 +7,19 @@ public class UserOfDLinkedList {
         linkedList.insertFirst(1);
         linkedList.insertFirst(2);
         linkedList.insertFirst(3);
+        linkedList.insertLast(10);
+        linkedList.insertLast(20);
+
+        linkedList.insertByPosition(30, 3);
+
+        linkedList.deleteFirst();
+        linkedList.deleteLast();
+
+        linkedList.deleteByPosition(3);
+
+        linkedList.reverse();
+
+        linkedList.empty();
 
         System.out.println(linkedList);
     }
